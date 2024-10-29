397abb38176e02bfa6dcd5289fdf84d9b3b88770
Sergio <ferrari8986@gmail.com>
2024-10-29 20:42:04 -0300

    changes on header css to not break on media querys

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,51 +9,53 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ setActiveTab }) => (
     <header className="flex flex-col rounded-t-[5%] text-white">
-        <div className="flex items-center w-full">
+        <div className="flex flex-col items-center lg:flex-row lg:items-start w-full">
             <Image
                 src="/profile.jpg"
                 alt="Profile Picture"
                 width={1080}
                 height={720}
-                className="rounded-full w-[23em] h-[23em] mr-4 sm:w-[20em] sm:h-[20em] xs:w-[15em] xs:h-[15em]"
+                className="rounded-full w-64 h-64 mb-4 sm:w-72 sm:h-72 md:w-80 md:h-80 lg:w-[23em] lg:h-[23em] lg:mr-4"
             />
-            <div className="font-['Segoe_UI',Tahoma,Geneva,Verdana,sans-serif] text-2xl shadow-[1px_0px_1px_#000,0px_1px_1px_#00000050,2px_1px_1px_#000,1px_2px_1px_#00000050,3px_2px_1px_#000,2px_3px_1px_#00000050,4px_3px_1px_#000,3px_4px_1px_#00000050,5px_4px_1px_#000,4px_5px_1px_#00000050,6px_5px_1px_#000,5px_6px_1px_#00000050,7px_6px_1px_#000] m-4 md:text-xl sm:absolute sm:right-10 sm:text-base xs:text-sm">
-                <h2 className="md:text-3xl">Hi! I{"'"}m</h2>
-                <h1 className="uppercase md:text-4xl">
+            <div className="font-['Segoe_UI',Tahoma,Geneva,Verdana,sans-serif] text-center lg:text-left text-xl shadow-[1px_0px_1px_#000,0px_1px_1px_#00000050,2px_1px_1px_#000,1px_2px_1px_#00000050,3px_2px_1px_#000,2px_3px_1px_#00000050,4px_3px_1px_#000,3px_4px_1px_#00000050,5px_4px_1px_#000,4px_5px_1px_#00000050,6px_5px_1px_#000,5px_6px_1px_#00000050,7px_6px_1px_#000] m-4 sm:text-2xl md:text-3xl lg:text-2xl">
+                <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-3xl">
+                    Hi! I{"'"}m
+                </h2>
+                <h1 className="uppercase text-4xl sm:text-5xl md:text-6xl lg:text-4xl">
                     Sergio
                     <br /> Ferrari
                     <br /> Bryce
                 </h1>
             </div>
         </div>
-        <nav className="flex items-start w-full mt-5">
+        <nav className="flex flex-wrap justify-center lg:justify-start items-start w-full mt-5">
             <button
                 onClick={() => setActiveTab("about")}
-                className="py-2.5 px-2.5 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c] rounded-tl-2xl"
+                className="py-2 px-2 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c] rounded-tl-2xl text-sm sm:text-base"
             >
                 Sobre mí
             </button>
             <button
                 onClick={() => setActiveTab("proyects")}
-                className="py-2.5 px-2.5 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c]"
+                className="py-2 px-2 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c] text-sm sm:text-base"
             >
                 Proyectos
             </button>
             <button
                 onClick={() => setActiveTab("skills")}
-                className="py-2.5 px-2.5 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c]"
+                className="py-2 px-2 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c] text-sm sm:text-base"
             >
                 Skills
             </button>
             <button
                 onClick={() => setActiveTab("pdf")}
-                className="py-2.5 px-2.5 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c]"
+                className="py-2 px-2 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c] text-sm sm:text-base"
             >
                 Curriculum
             </button>
             <button
                 onClick={() => setActiveTab("contact")}
-                className="py-2.5 px-2.5 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c] rounded-tr-2xl"
+                className="py-2 px-2 bg-black bg-opacity-80 text-white border border-black cursor-pointer hover:bg-[#8b631d7c] rounded-tr-2xl text-sm sm:text-base"
             >
                 Contacto
             </button>
