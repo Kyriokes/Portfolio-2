63fbb1cb02a49fb66e85cbc381c72fc74e204a53
Sergio <ferrari8986@gmail.com>
2025-05-09 05:45:17 -0300

    seo

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,30 @@ import Pdf from "./components/Pdf";
 import Skills from "./assets/skills";
 import { Proyects } from "./components/Proyects";
 
+export const metadata = {
+    title: "Inicio | Mi Portfolio",
+    description:
+        "Desarrollador web frontend con experiencia en React, Next.js y más.",
+    keywords: ["frontend", "desarrollador", "portfolio", "Next.js", "React","Sergio Ferrari","Sergio Ferrari Bryce"],
+    authors: [{ name: "Sergio Ferrari Bryce" }],
+    openGraph: {
+        title: "Inicio | Mi Portfolio",
+        description: "Conoce más sobre mi trabajo como desarrollador web.",
+        url: "https://portfoliosergiofb.vercel.app/",
+        siteName: "Mi Portfolio",
+        images: [
+            {
+                url: "https://portfoliosergiofb.vercel.app/profile.jpg",
+                width: 1200,
+                height: 630,
+                alt: "Captura del portfolio",
+            },
+        ],
+        locale: "es_ES",
+        type: "website",
+    },
+};
+
 export default function Home() {
     const [activeTab, setActiveTab] = useState<string>("about");
     const [transitioning, setTransitioning] = useState<boolean>(false);
